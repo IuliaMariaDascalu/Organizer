27d69cb2a8d615feca5b156d104ed21fed42a64b
IuliaMariaDascalu <104492394+IuliaMariaDascalu@users.noreply.github.com>
2022-04-27 12:09:33 +0300

    Update vizualizare_profil.php

--- TemplateProiect/pagini/conectat/vizualizare_profil.php
+++ TemplateProiect/pagini/conectat/vizualizare_profil.php
@@ -81,7 +81,15 @@ if (isset($_POST['actualizeaza'])) {
         print 'Parola actuala nu este corecta!';
     }
     }
-
+$phpFileUploadedErrors =  array (0 => 'There is no error, the file uploaded with success',
+    1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+    2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+    3 => 'The uploaded file was only partially uploaded',
+    4 => 'No file was uploaded',
+    6 => 'Missing a temporary folder',
+    7 => 'Failed to write file to disk.',
+    8 => 'A PHP extension stopped the file upload.',
+);
 if (isset($_POST['incarca'])) {
     
     if(isset($_FILES['img'])) {
